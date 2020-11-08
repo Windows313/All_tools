31f6566526dd21a659aeb762b51773406efeb72e
Windows313 <57021443+Windows313@users.noreply.github.com>
2020-11-08 09:58:56 +0200

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -12,4 +12,17 @@ pkg install curl -y
 gem install lolcat
 clear
 toilet -f pagga "Done..!!" | lolcat
-toilet -f pagga "Done..!!" | lolcat
+toilet -f pagga "Lb_cyber_team..!!" | lolcat
+toilet -f pagga "is..!:" | lolcat
+toilet -f pagga "windows 313..!!" | lolcat
+toilet -f pagga "Ahmad M..!!" | lolcat
+toilet -f pagga "Phoenix MR..!!" | lolcat
+toilet -f pagga "Dark Hacker..!!" | lolcat
+toilet -f pagga "Jelad Lb..!!" | lolcat
+toilet -f pagga "Shaba7 Ldwer..!!" | lolcat
+toilet -f pagga "kaawash..!!" | lolcat
+toilet -f pagga "Hussein J..!!" | lolcat
+toilet -f pagga "Kritos..!!" | lolcat
+toilet -f pagga "Safir Lb..!!" | lolcat
+toilet -f pagga "Thanks..!!" | lolcat
+toilet -f pagga "Continue..!!" | lolcat
