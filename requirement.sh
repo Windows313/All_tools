4dfbb94dea957d088d48ca0dbc31ecf6625f2439
Windows313 <57021443+Windows313@users.noreply.github.com>
2020-11-08 09:26:32 +0200

    Create requirement.sh

---
+++ requirement.sh
@@ -0,0 +1,15 @@
+#
+#lebanon_cyber_Team
+#
+
+pkg update
+pkg upgrade
+pkg install figlet -y
+pkg install toilet -y
+pkg install nano -y
+pkg install ruby -y
+pkg install curl -y
+gem install lolcat
+clear
+toilet -f pagga "Done..!!" | lolcat
+toilet -f pagga "Done..!!" | lolcat
